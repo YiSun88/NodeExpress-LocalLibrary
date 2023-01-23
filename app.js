ba3a95cda01bc9c011b7ad41dca619105980c9c0
Yi Sun <timyisun@gmail.com>
2023-01-22 18:43:32 -0600

    Removed node_modules folder for Production

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ const catalogRouter = require('./routes/catalog'); //Import routes for "catalog"
 var app = express();
 
 // Set up mongoose connection
-const mongoDB = "mongodb+srv://libAdmin:thisisfortest@mdn-library-example.bxwac5k.mongodb.net/local_library?retryWrites=true&w=majority";
+const dev_db_url = "mongodb+srv://libAdmin:thisisfortest@mdn-library-example.bxwac5k.mongodb.net/local_library?retryWrites=true&w=majority";
+
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
